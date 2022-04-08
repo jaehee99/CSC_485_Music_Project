b92c6792b55160615039ef998a22d290c95a4466
Jaehee Lee <jaehee0112@newjoeys.com>
2022-04-08 00:16:17 -0400

    update cleaning spotify_music_data
    Extract only 'year' from release_date variable and then assign it to new variable called "year" & cleaned the data in the appropriate format

--- data/spotify_data_cleaning.R
+++ data/spotify_data_cleaning.R
@@ -5,10 +5,24 @@ spotify_music_data <- readr::read_csv('https://raw.githubusercontent.com/rfordat
 library(tidyverse)
 spotify_music_data %>%  
   select(-c("track_id", "track_album_id", "playlist_id")) %>%  
-  rename(artist = track_artist, popularity = track_popularity, relese_date = track_album_release_date, genre = playlist_genre, subgenre = playlist_subgenre) -> spotify_music_data
+  rename(artist = track_artist, popularity = track_popularity, release_date = track_album_release_date, genre = playlist_genre, subgenre = playlist_subgenre) -> spotify_music_data
 
 colnames(spotify_music_data) # show the variables 
 str(spotify_music_data) # see the structure of the data
 
-# save it to csv 
-write_csv(spotify_music_data, "spotify_music_data.csv")
+# Extract only 'year' from release_date variable and then assign it to new variable called "year"
+spotify_music_data %>%
+  mutate(year = format(as.Date(release_date), "%Y")) -> updated_spotify_music_data
+sum(is.na(updated_spotify_music_data$year)) # There are 1886 NAs in this "year" variable 
+# The reason why there are NAs? It is because release_date variable has non-consistent format of date
+# For example, "2019-06-14" or "2011"
+
+updated_spotify_music_data$year <- ifelse(is.na(updated_spotify_music_data$year), 
+                                          updated_spotify_music_data$relese_date, 
+                                          updated_spotify_music_data$year) # Use ifelse statement to replace NAs with year values in release_date variable 
+sum(is.na(updated_spotify_music_data$year)) # check if it is resolved
+head(updated_spotify_music_data) # show first 6 rows to check how it is going 
+names(updated_spotify_music_data) # show the variable names 
+updated_spotify_music_data %>%  
+  select(c("track_name", "track_album_name", "playlist_name","artist", "popularity", "genre", "subgenre", "year", "danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms")) -> spotifyMusic
+write_csv(spotifyMusic, "spotifyMusic.csv")
