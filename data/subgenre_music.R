fd4154b8adeb2ea8ef2ac4163c9006d859c58ea6
Jaehee Lee <jaehee0112@newjoeys.com>
2022-05-02 01:40:23 -0400

    add new file

---
+++ data/subgenre_music.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+Spotify_Music <- read_csv("Spotify_Music.csv")
+
+Spotify_Music %>%  
+  group_by(genre, subgenre, year) %>%  
+  summarise(subgenre_mean = mean(popularity)) -> subgenre_music
+write.csv(subgenre_music, "subgenre_music.csv")
